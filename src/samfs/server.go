671a6ca6f5a3f70affe10586c9a9e79613361335
Arman Shanjani <armanshan12@gmail.com>
2016-10-07 16:27:39 -0500

    Implemented server Create()

--- src/samfs/server.go
+++ src/samfs/server.go
@@ -74,7 +74,35 @@ func (s *SamFSServer) Commit(ctx context.Context, req *pb.CommitRequest) (*pb.St
 }
 
 func (s *SamFSServer) Create(ctx context.Context, req *pb.LocalDirectoryRequest) (*pb.FileHandleReply, error) {
-	return nil, nil
+	directoryPath := path.Join(s.rootDirectory, req.DirectoryFileHandle.Path)
+	if _, err := os.Stat(directoryPath); os.IsNotExist(err) {
+		glog.Errorf("path %s does not exist :: %v\n", req.DirectoryFileHandle.Path, err)
+		return nil, err
+	}
+
+	//TODO (arman): check version number of local directory against db
+
+	fsFilePath := path.Join(req.DirectoryFileHandle.Path, req.Name)
+	//TODO (arman): add fsFilePath and its *new* version number to db
+
+	filePath := path.Join(directoryPath, req.Name)
+	file, err := os.Create(filePath)
+	if err != nil {
+		glog.Errorf("Failed to create file at path %s :: %v\n", filePath, err)
+		return nil, err
+	}
+	file.Close()
+
+	fileHandle := &pb.FileHandle{
+		Path:    fsFilePath,
+		Version: 0, //TODO (arman): return the new version number
+	}
+
+	resp := &pb.FileHandleReply{
+		FileHandle: fileHandle,
+	}
+
+	return resp, nil
 }
 
 func (s *SamFSServer) Remove(ctx context.Context, req *pb.LocalDirectoryRequest) (*pb.StatusReply, error) {
