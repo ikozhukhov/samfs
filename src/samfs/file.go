eadc9011b71bcc7d78208d9e839684c7ef5c619f
Mihir Shete <mihirsht@gmail.com>
2016-10-19 06:12:43 +0000

    samfs: add client cache for uncommited writes

--- src/samfs/file.go
+++ src/samfs/file.go
@@ -17,13 +17,24 @@ type SamFsFileHandle struct {
 	fileData *SamFsFileData
 }
 
+type CacheEntry struct {
+	Data            *[]byte
+	Offset          int64
+	ServerSessionID int64
+}
+
+type Cache struct {
+	entries    []*CacheEntry
+	numEntries uint32
+}
+
 type SamFsFileData struct {
 	sync.Mutex
 	serverFh *pb.FileHandle
 	Fs       *SamFs
 	Refs     int32
 	Name     string
-	Data     []byte
+	DCache   Cache
 	Dirty    bool
 	Attr     *fuse.Attr
 }
@@ -50,6 +61,21 @@ func NewFileData(path string, fs *SamFs, serverFh *pb.FileHandle) *SamFsFileData
 		Dirty:    false,
 		serverFh: serverFh,
 	}
+}
+
+func (cc *Cache) AddEntry(e *CacheEntry) {
+	cc.entries = append(cc.entries, e)
+	cc.numEntries += 1
+}
+
+func (cc *Cache) Invalidate() {
+	if cc.numEntries == 0 {
+		return
+	}
+	// copy is required so that gc can remove the older entries
+	// otherwise our array will keep on growing
+	cc.entries = []*CacheEntry{}
+	cc.numEntries = 0
 }
 
 func (c *SamFsFileHandle) String() string {
@@ -91,25 +117,37 @@ func (c *SamFsFileHandle) Write(data []byte, offset int64) (uint32,
 	fuse.Status) {
 
 	glog.V(3).Infof("Write called on %s", c.fileData.Name)
-	name := c.fileData.Name
-	fh := c.fileData.serverFh
-	_, err := c.fileData.Fs.nfsClient.Write(context.Background(),
-		&pb.WriteRequest{
-			FileHandle: fh,
-			Offset:     offset,
-			Size:       int64(len(data)),
-			Data:       data,
-		})
+
+	resp, err := c.write(data, offset)
+
 	if err != nil {
-		glog.Errorf(`failed to write to file "%s" :: %s`, name, err.Error())
+		glog.Errorf(`failed to write to file "%s" :: %s`, c.fileData.Name,
+			err.Error())
 		return 0, fuse.EIO
 	}
+	if c.fileData.DCache.numEntries != 0 &&
+		c.fileData.DCache.entries[c.fileData.DCache.numEntries-1].ServerSessionID !=
+			resp.ServerSessionID {
+		glog.Warning("server state change detected")
+		c.fileData.Dirty = true
+	}
+	c.fileData.Lock()
+	c.fileData.DCache.AddEntry(&CacheEntry{
+		Data:            &data,
+		Offset:          offset,
+		ServerSessionID: resp.ServerSessionID,
+	})
+	c.fileData.Unlock()
 	return uint32(len(data)), fuse.OK
 }
 
 func (c *SamFsFileHandle) Flush() fuse.Status {
 	glog.V(3).Infof("Flush called on %s", c.fileData.Name)
-	return fuse.OK
+	if c.fileData.DCache.numEntries == 0 {
+		return fuse.OK
+	} else {
+		return c.Fsync(0)
+	}
 }
 
 func (c *SamFsFileHandle) Allocate(off uint64, size uint64,
@@ -121,6 +159,9 @@ func (c *SamFsFileHandle) Allocate(off uint64, size uint64,
 
 func (c *SamFsFileHandle) Release() {
 	glog.V(3).Infof("Release called on %s", c.fileData.Name)
+	if c.fileData.DCache.numEntries != 0 {
+		_ = c.Fsync(0)
+	}
 	c.fileData.Lock()
 	c.fileData.Refs--
 	c.fileData.Unlock()
@@ -130,6 +171,62 @@ func (c *SamFsFileHandle) Release() {
 
 func (c *SamFsFileHandle) Fsync(flags int) fuse.Status {
 	glog.V(3).Infof("Fsync called %s", c.fileData.Name)
+	if c.fileData.Dirty == true {
+		glog.Errorf("Fsync called on %s with dirty cache", c.fileData.Name)
+	}
+
+	fh := c.fileData.serverFh
+	var resp *pb.StatusReply
+	var err error
+	if c.fileData.Dirty != true {
+		resp, err = c.fileData.Fs.nfsClient.Commit(context.Background(),
+			&pb.CommitRequest{
+				FileHandle: fh,
+			})
+		if err != nil {
+			glog.Errorf(`failed to commit to file "%s" :: %s`, c.fileData.Name,
+				err.Error())
+			return fuse.EIO
+		}
+	}
+
+	// crash detected between writes or between fsync and writes
+	if (c.fileData.DCache.numEntries != 0 &&
+		c.fileData.DCache.entries[c.fileData.DCache.numEntries-1].ServerSessionID !=
+			resp.ServerSessionID) || (c.fileData.Dirty == true) {
+		glog.Warning("server state change detected during fsync, replay all writes")
+
+		for _, de := range c.fileData.DCache.entries {
+			resp, err := c.write(*de.Data, de.Offset)
+			if err != nil {
+				glog.Errorf(`failed to write during recovery to file "%s" :: %s`,
+					c.fileData.Name, err.Error())
+				return fuse.EIO
+			}
+
+			de.ServerSessionID = resp.ServerSessionID
+		}
+		resp, err := c.fileData.Fs.nfsClient.Commit(context.Background(),
+			&pb.CommitRequest{
+				FileHandle: fh,
+			})
+		if err != nil {
+			glog.Errorf(`failed to recursively commit to file "%s" :: %s`,
+				c.fileData.Name, err.Error())
+			return fuse.EIO
+		}
+		for _, de := range c.fileData.DCache.entries {
+			if de.ServerSessionID != resp.ServerSessionID {
+				glog.Error("recursive crash during fsync")
+				return fuse.EIO
+			}
+		}
+	}
+
+	c.fileData.Lock()
+	c.fileData.DCache.Invalidate()
+	c.fileData.Unlock()
+
 	return fuse.OK
 }
 
@@ -191,4 +288,23 @@ func (c *SamFsFileHandle) Utimens(atime *time.Time,
 
 	glog.V(3).Infof("Utimens(file) called %s", c.fileData.Name)
 	return fuse.OK
+}
+
+func (c *SamFsFileHandle) write(data []byte, offset int64) (*pb.StatusReply,
+	error) {
+
+	glog.V(3).Infof("Write called on %s", c.fileData.Name)
+	fh := c.fileData.serverFh
+
+	c.fileData.Lock()
+	resp, err := c.fileData.Fs.nfsClient.Write(context.Background(),
+		&pb.WriteRequest{
+			FileHandle: fh,
+			Offset:     offset,
+			Size:       int64(len(data)),
+			Data:       data,
+		})
+	c.fileData.Unlock()
+
+	return resp, err
 }
